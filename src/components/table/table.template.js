5f3563ecc76c80b08526e7b7e584cc38311943ac
id.v1cher <id.v1cher@gmail.com>
2021-03-29 11:20:58 +0500

    finish table resize

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,26 +3,34 @@ const CODES = {
     Z: 90
 }
 
-function createCell() {
+function createCell(_, col) {
     return `
-        <div class="cell" contenteditable></div>
+        <div class="cell" contenteditable data-col="${col}"></div>
     `
 }
 
-function createCol(el) {
+function createCol(col, index) {
     return `
-        <div class="column">${el}</div>
+        <div class="column" data-type="resizable" data-col="${index}">
+            ${col}
+            <div class="col-resize" data-resize="col"></div>
+        </div>
     `
 }
 
 function createRow(index, content) {
+    const resizer = index ? '<div class="row-resize" data-resize="row"></div>' : ''
     return `
-        <div class="row">
-            <div class="row-info">${index ? index : ''}</div>
+        <div class="row" data-type="resizable">
+            <div class="row-info">
+                ${index ? index : ''}
+                ${resizer}
+            </div>
             <div class="row-data">${content}</div>
         </div>
     `
 }
+
 
 export function createTable(rowsCount = 150) {
     const colsCount = CODES.Z - CODES.A + 1
@@ -30,8 +38,8 @@ export function createTable(rowsCount = 150) {
 
     const cols = new Array(colsCount).fill('').map((el, index) => {
         return String.fromCharCode(CODES.A + index)
-    }).map((el) => {
-        return createCol(el)
+    }).map((el, index) => {
+        return createCol(el, index)
     }).join('')
 
     rows.push(createRow(null, cols))
